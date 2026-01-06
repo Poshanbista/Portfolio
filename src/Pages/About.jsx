a5d65f7171aee7a37036075b472f254a968fb785
Poshan Bista <bistaposhan123@gmail.com>
2026-01-06 16:17:21 +0545

    update in education page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -18,17 +18,15 @@ export default function About() {
 
           <p className='font-semibold text-2xl mt-4'>Full Stack Developer</p>
 
-          <p className='text-xl mt-4 text-gray-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Quis distinctio modi, mollitia velit sapiente excepturi qui
-            beatae ea reprehenderit dolorum neque minima voluptatem
-            consequatur provident laudantium. Dolor ab debitis blanditiis impedit.
-            Dicta praesentium rerum atque, corrupti ea aliquam suscipit fugit
-            eos tempora provident earum blanditiis quia eligendi neque impedit
-            modi dolore quaerat architecto quod aut at quis nostrum aperiam!
-            Ratione dolorum maxime quas eius. Deleniti accusantium corrupti
-            tempora impedit a amet odio! Voluptates temporibus earum esse
-            similique laborum ea magni, tempora ex vel eveniet quo nostrum, atque iste, eaque ad quidem.
-            Voluptates debitis deleniti exercitationem animi unde in aliquid voluptatibus!</p>
+          <p className='text-xl mt-4 text-gray-600'>I have some experience as a full stack
+            developer from internships and stuff I built on my own. It feels good to work
+            on web apps that actually respond well and can handle more users,using things
+            like React.js for the front and Node.jswith Express.js on the back end. MongoDB
+            comes in for the database part too.I get the basics of frontend pretty well, HTML
+            and CSS are straightforward, and I like how Tailwind CSS makes styling faster, plus
+            JavaScript ties it alltogether. Backend wise, Ive done RESTful APIs and figured out
+            authentication, which was tricky at first. Database design is something Im still
+            getting comfortable with.</p>
 
           <div className='mt-8 flex gap-8 '>
             <a href='#contact'
--- src/Pages/Education.jsx
+++ src/Pages/Education.jsx
@@ -1,10 +1,88 @@
 import React from 'react'
 
 export default function Education() {
+
+  const education = [
+    {
+      degree: "Bachelor of computer Application (BCA)",
+      institute: "Patan Multiple Campus",
+      year: "2021 – Present",
+      location: "Patandhoka, lalitpur Nepal"
+    },
+    {
+      degree: "+2",
+      institute: "Gorkha College Dang",
+      year: "Completed",
+      location: "Tulsipur, Dang Nepal"
+    },
+    {
+      degree: "SEE",
+      institute: "Deep Jyoti Vidyashram",
+      year: "Completed",
+      location: "Chapergaudi, Banke Nepal"
+    }
+  ]
+
+  const experience = [
+    {
+      role: "Full Stack Intern",
+      company: "NepDev Technology",
+      year: "2025 – Present",
+      description: "Worked on MERN stack projects."
+    },
+    {
+      role: "DevOps Intern",
+      company: "NepDev Technology",
+      year: "1 month",
+      description: "Deploy Website"
+    }
+  ];
+
   return (
     <section id='education' className='mb-10'>
-      <p>Education page</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste fugiat commodi quo ab, aspernatur eius quidem quas nisi consequuntur officiis explicabo expedita libero ipsam, facilis harum ex optio illum inventore nihil perspiciatis beatae dolor labore. Dolorum voluptatum dolores fugiat, velit temporibus quaerat quam. Ea porro veritatis, numquam magnam id libero quos reprehenderit reiciendis inventore quasi minima corporis doloremque, dolore esse! Nobis accusantium et dolorem veritatis alias, similique inventore pariatur mollitia ipsum eius nesciunt tenetur aliquam totam beatae quas, saepe officia distinctio illo eligendi expedita at earum incidunt. Dolores assumenda rem ab architecto sapiente? Doloremque error quidem perferendis sequi fuga.</p>
+      <div className='ml-6 mr-6'>
+        <h1 className='font-semibold text-4xl text-center'>My Qualification</h1>
+        <div className='flex justify-center mt-4'>
+          <div className='w-32 h-1 bg-blue-500  rounded-full'></div>
+        </div>
+
+        <h2 className="text-2xl font-semibold mb-6">🎓 Education</h2>
+
+        <div className="grid md:grid-cols-2 gap-6">
+          {education.map((edu, index) => (
+            <div
+              key={index}
+              className="border rounded-lg p-5 hover:shadow-md transition"
+            >
+              <h3 className="text-lg font-semibold">{edu.degree}</h3>
+              <p className="text-gray-600">{edu.institute}</p>
+              <p className="text-sm text-gray-500">{edu.year}</p>
+              <p className="mt-2 text-gray-700">{edu.description}</p>
+            </div>
+          ))}
+        </div>
+
+        {/* Experience */}
+        <div className="mt-4">
+          <h2 className="text-2xl font-semibold mb-6">💼 Experience</h2>
+
+          <div className="grid grid-cols-2 gap-6">
+            {experience.map((exp, index) => (
+              <div
+                key={index}
+                className="border rounded-lg p-5 hover:shadow-md transition"
+              >
+                <h3 className="text-lg font-semibold">{exp.role}</h3>
+                <p className="text-gray-600">{exp.company}</p>
+                <p className="text-sm text-gray-500">{exp.year}</p>
+                <p className="mt-2 text-gray-700">{exp.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+      </div>
     </section>
   )
 }
